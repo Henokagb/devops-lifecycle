e0c4856827674a03e7112dc34db89314824f0785
Hénok <72027682+Henokagb@users.noreply.github.com>
2024-11-07 17:44:05 +0100

    Update settings.py

--- userapi/project/settings.py
+++ userapi/project/settings.py
@@ -56,6 +56,12 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_HEADERS = [
+    "X-CSRFTOKEN"
+]
+
 
 ROOT_URLCONF = 'project.urls'
 
@@ -129,8 +135,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "DELETE"]
-
