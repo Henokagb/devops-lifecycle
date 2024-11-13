b6abb35f192f09fe26c7c6dec3557922fb5302dd
Hénok <72027682+Henokagb@users.noreply.github.com>
2024-11-13 18:06:47 +0100

    Update urls.py

--- userapi/api/urls.py
+++ userapi/api/urls.py
@@ -15,7 +15,7 @@ schema_view = get_schema_view(
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
-   url='https://devops-lifecycle.onrender.com/api/'
+   #url='https://devops-lifecycle.onrender.com/api/'
 )
 
 
